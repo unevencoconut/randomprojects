4305f311c2ab74bbdcff74284e2cf254c6e62fd9
Mark SIlva <marksilva@Marks-Work-Lappy-Pro.local>
2020-10-07 13:53:35 -0700

    Heard about the Stimulus Framework, figured I'd try it out

---
+++ src/controllers/hello_controller.js
@@ -0,0 +1,8 @@
+// src/controllers/hello_controller.js
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+    connect(){
+        console.log("Hello, Stimulus!", this.element)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,94 @@
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+    (() => {
+      const application = Stimulus.Application.start()
 
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+      application.register("clipboard", class extends Stimulus.Controller {
+        static get targets() {
+          return [ "source" ]
+        }
+
+        copy(){
+          event.preventDefault();
+          this.sourceTarget.select();
+          document.execCommand("copy");
+        }
+
+        connect(){
+          if (document.queryCommandSupported("copy")){
+            this.element.classList.add("clipboard--supported");
+          }
+        }
+      });
+
+      application.register("slideshow", class extends Stimulus.Controller {
+        static get targets() {
+          return [ "slide" ]
+        };
+
+        initialize() {
+          this.showCurrentSlide();
+          const index = parseInt(this.data.get("index"));
+          this.showSlide(index);
+
+          // Testing the Data API from Stumulis
+          console.log( this.data.has("index") ) // Returns true if the Controller Ele has a Data-Slideshow-Index Attribute
+          console.log( this.data.get("index") ) // returns the string value of the ele data-slideshow-index attribute
+          console.log( this.data.set("index", index ) ) // sets the elements data-slideshow-index attribute to the string value of index
+        };
+
+        next() {
+          let isCeiling = this.index;
+          isCeiling != 3 ? this.index++ : null ;
+        };
+
+        previous() {
+          let isFloor = this.index;
+          isFloor != 0 ? this.index-- : null ;
+        };
+
+        showCurrentSlide(){
+          this.slideTargets.forEach((el,i) => {
+            el.classList.toggle("slide--current", this.index == i);
+          });
+        }
+
+        get index() {
+          return parseInt(this.data.get("index"));
+        }
+
+        set index(value){
+          this.data.set("index", value);
+          this.showCurrentSlide();
+        }
+
+        // Previous Method in the tutorial, then refactored
+        showSlide(index) {
+          this.index = index;
+          this.slideTargets.forEach((el, i) => {
+            el.classList.toggle("slide--current", index == i);
+          })
+        }
+      });
+
+      application.register("content-loader", class extends Stimulus.Controller {
+          connect(){
+              this.load();
+          };
+
+          load(){
+            //   console.log(this.data.get("url"));
+              const messagesURL = "../public/messages.html";
+              fetch(messagesURL)
+                .then(response => response.text())
+                .then(html => {
+                    this.element.innerHTML = html 
+                })
+          }
+      })
+    })()
+
+// import { Application } from "stimulus"
+// import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+// const application = Application.start()
+// const context = require.context("./controllers", true, /\.js$/)
+// application.load(definitionsFromContext(context))
